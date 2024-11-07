4302577954abdd6ecc8215ff9a5bcf634c37ee1c
NicolasJ88 <168785550+NicolasJ88@users.noreply.github.com>
2024-11-07 19:49:40 -0300

    Add files via upload

---
+++ newtp/proyecto-1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavBar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar titulo="Bienvenido"/>
+    </>
+  )
+}
+
+export default App
---
+++ newtp/proyecto-1/src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+const NavBar = (props) => {
+    return <nav style={{color: 'Blue'}}>{props.titulo}</nav>;
+  }
+  
+  export default NavBar;
